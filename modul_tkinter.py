500ee267e6401af499143cc77f1239a251144ce3
aleksandr_markhel <volatpy@gmail.com>
2023-06-28 23:26:13 +0300

    winfo_

--- modul_tkinter.py
+++ modul_tkinter.py
@@ -1,14 +1,28 @@
 from tkinter import *
 from tkinter import ttk
 
-
 root = Tk()
 root.title("My GUI project")
-root.geometry("600x400+1200+100")
-root.resizable(width=False, height=False)
 root.iconbitmap("./python_icon.ico")
-root_icon = PhotoImage(file="./python.png")
-# root.iconphoto(False, root_icon)
+root.geometry("600x400+200+150")
+
+def print_text():
+    print("Hello, you ckicked the button 'Enter'")
+enter_button = Button(
+    root,
+    text="Enter",
+    bg="#FFFAD7",
+    font=("Lucida Console", 16, "normal"),
+    fg="green",
+    relief="raised",
+    activebackground="yellow",
+    activeforeground="red",
+    command=print_text
+)
+enter_button.pack()
+
+label = Label(root, text=enter_button.winfo_rootx())
+label.pack()
 
 
 
