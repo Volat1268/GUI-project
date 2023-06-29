214f5433c328425997efdfe19b3bbfe083440900
aleksandr_markhel <volatpy@gmail.com>
2023-06-29 23:19:27 +0300

    name_label

--- knb.py
+++ knb.py
@@ -3,6 +3,19 @@ from tkinter import *
 root = Tk()
 root.title("Камень, ножницы, бумага.")
 root.iconbitmap("./assets/knb_image.ico")
+ROOT_WIGHT = 600
+ROOT_HIGHT = 400
+ROOT_XCOR = 200
+ROOT_YCOR = 200
+FONT_LABEL = ("Helvetica", 14, "bold italic")
+FONT_BUTTON = ("Helvetica", 12, "normal")
+
+nambe_Label = Label(root,
+                    text="🤞 Давай поиграем в\nКамень, Ножницы, Бумага!",
+                    font=FONT_LABEL,
+                    
+                    )
+nambe_Label.pack()
 
 
 
