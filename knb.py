71127bf42f4ac3cd48d3fb98c0de11bf3232cf4f
Aleks_work <alemarkhel@yandex.ru>
2023-06-30 14:15:17 +0300

    buttons added

--- knb.py
+++ knb.py
@@ -9,16 +9,42 @@ ROOT_XCOR = 200
 ROOT_YCOR = 200
 FONT_LABEL = ("Helvetica", 14, "bold italic")
 FONT_BUTTON = ("Helvetica", 12, "normal")
+player_score = 0
+comp_score = 0
 
-nambe_Label = Label(root,
+
+# -----root (window of game)----------------------
+root.geometry("{}x{}+{}+{}".format(ROOT_WIGHT, ROOT_HIGHT, ROOT_XCOR, ROOT_YCOR))
+
+
+# --------------labels:---------------------------
+title_Label = Label(root,
                     text="🤞 Давай поиграем в\nКамень, Ножницы, Бумага!",
-                    font=FONT_LABEL,
-                    
+                    font=FONT_LABEL,                    
                     )
-nambe_Label.pack()
+title_Label.pack()
 
 
+score_label = Label(root,
+                    text=f"СЧЕТ: компьютер {comp_score} : игрок {player_score},"
+                    )
+score_label.pack()
 
+# TOFIX:
+# img = PhotoImage(True, file="./assets/knb_image.jpg")
+# image_label = Label(root, image=img,                    
+#                     )
+# image_label.pack()
+
+# --------------buttons:---------------------------
+stone_button = Button(text="камень")
+stone_button.pack()
+
+scissors_button = Button(text="ножницы")
+scissors_button.pack()
+
+paper_button = Button(text="бумага")
+paper_button.pack()
 
 
 root.mainloop()
