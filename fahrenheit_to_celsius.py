fa92f4b74929131b7e093bc0815b56308a8cfbed
aleksandr_markhel <volatpy@gmail.com>
2023-07-01 23:18:06 +0300

    structur of project added

--- fahrenheit_to_celsius.py
+++ fahrenheit_to_celsius.py
@@ -1,2 +1,35 @@
 from tkinter import *
 
+root = Tk()
+root.title("fahrenheit to celsius")
+
+fahrenheit_var = StringVar()
+celsius_var = StringVar()
+
+fahrenheit_label = Label(root,
+                        text="Enter Fahrenheit:" )
+fahrenheit_enter = Entry(root,
+                         borderwidth=1,
+                         relief="solid",
+                         width=20,                         
+                         textvariable=fahrenheit_var,
+                         )
+tocelcius_button = Button(root,
+                          text="Convert")
+celsius_label = Label(root,
+                      text="Celsius temperatur")
+result_label = Label()
+
+fahrenheit_label.grid(row=0, column=0)
+fahrenheit_enter.grid(row=0, column=1)
+celsius_label.grid(row=1, column=0)
+result_label.grid(row=1, column=1)
+tocelcius_button.grid(row=2, column=0)
+
+
+
+root.mainloop()
+
+
+
+
