f0baa68ff53d7c05153eedac271030c267ba9495
Aleks_work <alemarkhel@yandex.ru>
2023-07-04 16:22:00 +0300

    random color maker

--- canvas_tk.py
+++ canvas_tk.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random as rd
 
 root = Tk()
 root.title("canvas Tk")
@@ -6,20 +7,28 @@ root.title("canvas Tk")
 class MyCanvas(Canvas):
     def __init__(self, master):
         super().__init__()
-        self["width"] = 200
-        self["height"] = 200
-        # self["bg"] = "gray"
+        self["width"] = 500
+        self["height"] = 500
         self["bd"] = 5
         self["relief"] = RAISED
 
-my_canvas_1 = MyCanvas(root)
-my_canvas_2 = MyCanvas(root)
-my_canvas_2.configure(bg='red', relief=SUNKEN)
+for_color = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+def random_color_maker():
+    random_color = "#"
+    for i in range(6):
+        random_color += rd.choice(for_color)
+    return random_color
 
-my_canvas_1.create_line(0, 0, 250, 250, fill="blue", width=5)
-my_canvas_2.create_line(210, 0, 0, 210, fill="yellow", width=5)
 
-my_canvas_1.grid(row=0, column=0)
-my_canvas_2.grid(row=1, column=1)
+my_canvas = MyCanvas(root)
+my_canvas.grid(row=0, column=0)
+
+for i in range(200):
+    my_canvas.create_line(rd.randint(0, 500), rd.randint(0, 500), rd.randint(0, 500), rd.randint(0, 500), fill=random_color_maker(), width=rd.randint(1, 10))
+    
+    my_canvas.update()
+
+
+
 
 root.mainloop()
